ffb8cfd9ac78d65b2e8bc2a94d2bdc82228a9c51
yunyeogyeong <dbsdurud456@naver.com>
2018-06-26 09:48:58 +0900

    첫번째 커밋

---
+++ addr/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Address(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    name = models.CharField(max_length=10)
+    call_1 = models.CharField(max_length=20)
+    call_2 = models.CharField(max_length=20, blank=True, null=True)
+    e_mail = models.EmailField(max_length=50, blank=True, null=True)
+    created_date = models.DateTimeField(
+        default=timezone.now)
+
+    def publish(self):
+        self.created_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.name
