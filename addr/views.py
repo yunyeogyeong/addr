49d9452f1ceb6e6f3c2d752e4e0d35fbb0102abb
yunyeogyeong <dbsdurud456@naver.com>
2018-06-26 10:21:23 +0900

    addr_list템플릿 설정

--- addr/views.py
+++ addr/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Address
 
 def addr_list(request):
-    return render(request, 'addr/addr_list.html', {})
+    addrs = Address.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+    return render(request, 'addr/addr_list.html', {'addrs': addrs})
